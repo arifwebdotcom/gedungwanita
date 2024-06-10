2365fb784a29a67b099b160a9c329456451eb51d
proquaconsulting <proqua.simrs@gmail.com>
2024-06-10 13:33:57 +0700

    as

--- app/Views/master/user/index.php
+++ app/Views/master/user/index.php
@@ -46,7 +46,7 @@ $breadcrumb_items = [
                                 <input class="form-check-input" type="checkbox" value="1" data-kt-check="true" data-kt-check-target=".widget-13-check">
                             </div>
                         </th>
-                        <th class="ps-4 ">Nama Peternak</th>
+                        <th class="ps-4 ">Nama Peternak</th> 
                         <th class="ps-4 ">Nama Peternakan</th>
                         <th class="ps-4 ">No HP</th>
                         <th class="ps-4 ">Alamat</th>
