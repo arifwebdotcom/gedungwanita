697339a35359a9813394890c586982c46cc0e97e
proquaconsulting <proqua.simrs@gmail.com>
2024-06-30 17:05:10 +0700

    edit user

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -53,7 +53,7 @@ $routes->get('/', 'Home::index', ['filter' => 'login','as' => 'home']);
 
 $routes->group('profile', ['filter' => 'login'],function ($routes) {
     $routes->get('/', [ProfileController::class, 'index'], ['as' => 'profile.index']);
-    $routes->post('editharga', [ProfileController::class, 'update'], ['as' => 'user.update']);
+    $routes->post('editharga', [ProfileController::class, 'update'], ['as' => 'profile.update']);
 });
 
 $routes->group('agama', ['filter' => 'login'],function ($routes) {
